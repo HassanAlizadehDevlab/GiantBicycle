97563fabcb5c9646e559dbeb56fc243a89bc121e
HassanAlizadeh <hassan.alizadeh529@gmail.com>
2021-02-28 22:13:44 +0330

    check username and password are not null

--- app/src/main/java/com/android/giantbicycle/login/domain/usecase/LoginUseCase.kt
+++ app/src/main/java/com/android/giantbicycle/login/domain/usecase/LoginUseCase.kt
@@ -1,21 +1,29 @@
 package com.android.giantbicycle.login.domain.usecase
 
 import com.android.giantbicycle.login.domain.repository.LoginRepository
+import com.android.shared.domain.string.StringBuilder
 import com.android.shared.domain.usecase.AsyncUseCase
 import io.reactivex.Single
 
+
 class LoginUseCase(
     private val repository: LoginRepository,
+    private val stringBuilder: StringBuilder,
 ) : AsyncUseCase<LoginUseCaseModel, Single<LoginUseCaseResult>> {
 
     override fun execute(param: LoginUseCaseModel): Single<LoginUseCaseResult> {
+        if (param.username.isNullOrEmpty())
+            return Single.just(LoginUseCaseResult(error = stringBuilder.usernameIsNull()))
+        if (param.password.isNullOrEmpty())
+            return Single.just(LoginUseCaseResult(error = stringBuilder.passwordIsNull()))
+
         return repository.login(
-            username = param.username!!,
-            password = param.password!!,
+            username = param.username,
+            password = param.password,
         ).map {
             LoginUseCaseResult(
                 result = it.result,
-                error = it.error?.name
+                error = stringBuilder.getMessageByError(it.error)
             )
         }
     }
--- app/src/test/java/com/android/giantbicycle/login/domain/usecase/LoginUseCaseTest.kt
+++ app/src/test/java/com/android/giantbicycle/login/domain/usecase/LoginUseCaseTest.kt
@@ -2,6 +2,7 @@ package com.android.giantbicycle.login.domain.usecase
 
 import com.android.giantbicycle.login.domain.repository.LoginRepository
 import com.android.shared.data.error.ApiResponse
+import com.android.shared.domain.string.StringBuilder
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
@@ -14,7 +15,8 @@ import org.junit.runners.JUnit4
 class LoginUseCaseTest {
 
     private val repository: LoginRepository = mockk()
-    private val usecase: LoginUseCase = LoginUseCase(repository)
+    private val stringBuilder: StringBuilder = mockk()
+    private val usecase: LoginUseCase = LoginUseCase(repository, stringBuilder)
 
 
     @Test
@@ -29,6 +31,7 @@ class LoginUseCaseTest {
                 error = null
             )
         )
+        every { stringBuilder.getMessageByError(null) } returns null
 
         usecase.execute(request)
             .test()
@@ -37,6 +40,58 @@ class LoginUseCaseTest {
             .assertValue(expected)
 
         verify { repository.login(username, password) }
+    }
+
+
+    @Test
+    fun `login user with empty or null username - error`() {
+        val username = null
+        val password = "password"
+        val request = LoginUseCaseModel(username, password)
+        val expectedError = "Please fill the username field"
+        val expectedResult = LoginUseCaseResult(result = null, error = expectedError)
+        every { repository.login(any(), any()) } returns Single.just(
+            ApiResponse(
+                result = null,
+                error = null
+            )
+        )
+        every { stringBuilder.usernameIsNull() } returns expectedError
+
+
+        usecase.execute(request)
+            .test()
+            .assertComplete()
+            .assertNoErrors()
+            .assertValue(expectedResult)
+
+
+        verify(exactly = 0) { repository.login(any(), any()) }
+    }
+
+    @Test
+    fun `login user with empty or null password - error`() {
+        val username = "username"
+        val password = null
+        val request = LoginUseCaseModel(username, password)
+        val expectedError = "Please fill the password field"
+        val expected = LoginUseCaseResult(result = null, error = expectedError)
+        every { repository.login(any(), any()) } returns Single.just(
+            ApiResponse(
+                result = null,
+                error = null
+            )
+        )
+        every { stringBuilder.passwordIsNull() } returns expectedError
+
+
+        usecase.execute(request)
+            .test()
+            .assertComplete()
+            .assertNoErrors()
+            .assertValue(expected)
+
+        verify(exactly = 0) { repository.login(any(), any()) }
     }
 
 }
